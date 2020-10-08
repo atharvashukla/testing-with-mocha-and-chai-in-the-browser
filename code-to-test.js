60da13d07565b61f773807a36ae4f99ea0cb5c60
Atharva Shukla <33644308+atharvashukla@users.noreply.github.com>
2020-10-08 08:02:50 +0530

    Add files via upload

---
+++ code-to-test.js
@@ -0,0 +1,3 @@
+function add3(z) {
+	return z + 3;
+}
---
+++ tests.js
@@ -0,0 +1,7 @@
+var assert = chai.assert
+
+describe('testing add3', () => {
+    it('add3 on 0 is 3', () => {
+        assert(add3(0) === 3);
+    });
+});
